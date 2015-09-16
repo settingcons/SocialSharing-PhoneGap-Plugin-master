39fb423b6f5b3b3c78e2dc302b6209db05643a4e
settingcons <settingcons@gmail.com>
2015-09-16 14:37:28 +0200

    20150916_18

--- src/ios/SocialSharing.m
+++ src/ios/SocialSharing.m
@@ -467,12 +467,6 @@
        _documentInteractionController.UTI = @"net.whatsapp.image";
        [_documentInteractionController presentOpenInMenuFromRect:CGRectMake(0, 0, 0, 0) inView:self.viewController.view animated: YES];
 
-//       NSString * savePath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/whatsAppTmp.waa"];
-//       [UIImageJPEGRepresentation(image, 1.0) writeToFile:savePath atomically:YES];
-//       _documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:savePath]];
-//       _documentInteractionController.UTI = @"net.whatsapp.audio";
-//       [_documentInteractionController presentOpenInMenuFromRect:CGRectMake(0, 0, 0, 0) inView:self.viewController.view animated: YES];
-	   
 	  
 //		// CAMBIO A AUDIO FILE
 //		// UIImage * iconImage = [UIImage imageNamed:@"myphoto.jpg"];
@@ -483,8 +477,7 @@
 //		_documentationInteractionController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:savePath]];
 //		_documentationInteractionController.UTI = @"net.whatsapp.audio";
 //		_documentationInteractionController.delegate = self;
-
-    [_documentationInteractionController presentOpenInMenuFromRect:CGRectMake(0, 0, 0, 0) inView:self.view animated: YES];
+//    [_documentationInteractionController presentOpenInMenuFromRect:CGRectMake(0, 0, 0, 0) inView:self.view animated: YES];
 	  
     } else {
       // append an url to a message, if both are passed
